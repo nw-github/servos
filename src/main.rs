7cbf0b59abaca19e540f106c4a9c9d0906c33fa8
Nathaniel W. <52637288+nw-github@users.noreply.github.com>
2024-07-12 12:38:17 -0400

    experimenting with the console

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@
 
 use core::{mem::MaybeUninit, ptr::addr_of};
 
+use arrayvec::{ArrayString, ArrayVec};
 use config::{HART_STACK_LEN, MAX_CPUS};
 use fdt_rs::base::DevTree;
 use servos::riscv;
@@ -75,15 +76,65 @@ extern "C" fn kmain(hartid: usize, fdt: *const u8) -> ! {
         unsafe { addr_of!(_kernel_end) },
     );
 
-    static mut BUFFER: [u8; 0x4000] = [0; 0x4000];
+    // static mut BUFFER: [u8; 0x4000] = [0; 0x4000];
+    // let dt = unsafe { DevTree::from_raw_pointer(fdt) }.unwrap();
+    // dump_fdt::dump_tree(dt, unsafe { &mut BUFFER[..] }).unwrap();
 
-    let dt = unsafe { DevTree::from_raw_pointer(fdt) }.unwrap();
-    unsafe { dump_fdt::dump_tree(dt, &mut BUFFER[..]) }.unwrap();
+    // println!("\n----");
+    // loop {
+    //     if let Some(ch) = SbiConsole::read() {
+    //         println!("{ch:#02x}");
+    //     }
+    // }
 
-    println!("\n----");
+    let mut cmd = ArrayString::<256>::new();
+    let mut buf = ArrayVec::<u8, 4>::new();
+    print!("\n>> ");
     loop {
-        if let Some(ch) = SbiConsole::read() {
-            println!("{ch:#02x}: {}", ch as char);
+        let ch = loop {
+            if let Some(b) = SbiConsole::read() {
+                break b;
+            }
+        };
+        match ch {
+            0x0d => {
+                print!("\nCommand: '{cmd}'\n>> ");
+                cmd.clear();
+                buf.clear();
+            }
+            0x7f => {
+                /* carriage return (sent on backspace) */
+                if !cmd.is_empty() {
+                    cmd.truncate(cmd.len() - cmd.chars().last().map(|c| c.len_utf8()).unwrap_or(0));
+                    print!("\x08 \x08");
+                }
+            }
+            0x17 => {
+                /* CTRL + backspace */
+                if let Some(&last) = cmd.as_bytes().last() {
+                    let pos = cmd
+                        .bytes()
+                        .rev()
+                        .position(|c| if last == b' ' { c != b' ' } else { c == b' ' })
+                        .map(|p| cmd.len() - p)
+                        .unwrap_or(0);
+                    cmd[pos..].chars().for_each(|_| print!("\x08 \x08"));
+                    cmd.truncate(pos);
+                }
+            }
+            ch if !ch.is_ascii_control() => {
+                if buf.try_push(ch).is_err() {
+                    buf.clear();
+                } else if let Ok(s) = core::str::from_utf8(&buf) {
+                    print!("{s}");
+                    if cmd.try_push_str(s).is_err() {
+                        print!("\nToo long! Command: '{cmd}'\n>> ");
+                        cmd.clear();
+                    }
+                    buf.clear();
+                }
+            }
+            _ => {}
         }
     }
 }
