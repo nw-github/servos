9de751e7ac250bf7bdbe3112ce3434423adad969
Nathaniel W. <52637288+nw-github@users.noreply.github.com>
2024-07-17 02:33:23 -0400

    find syscon from devicetree

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,11 @@ use core::{mem::MaybeUninit, ptr::addr_of};
 
 use arrayvec::{ArrayString, ArrayVec};
 use config::{HART_STACK_LEN, MAX_CPUS};
-use fdt_rs::base::DevTree;
+use fdt_rs::{
+    base::DevTree,
+    error::DevTreeError,
+    prelude::{FallibleIterator, PropReader},
+};
 use servos::riscv;
 use uart::SbiConsole;
 
@@ -19,7 +23,7 @@ mod uart;
 #[repr(C, align(16))]
 pub struct Align16<T>(pub T);
 
-static mut KSTACK: Align16<MaybeUninit<[[u8; HART_STACK_LEN]; MAX_CPUS]>> =
+static mut KSTACK: Align16<MaybeUninit<[[u8; HART_STACK_LEN]; MAX_CPUS + 1]>> =
     Align16(MaybeUninit::uninit());
 
 #[inline(always)]
@@ -52,9 +56,9 @@ extern "C" fn _start() -> ! {
             mv      tp, a0
             la      sp, {stack}
             li      t0, {stack_len}
-            addi    t1, a0, 1
-            mul     t0, t0, t1
+            add     t0, t0, t0
             add     sp, sp, t0
+            mv      a2, sp
             tail    {init}",
 
             init = sym kmain,
@@ -69,16 +73,72 @@ extern "C" {
     static _kernel_end: u8;
 }
 
-extern "C" fn kmain(hartid: usize, fdt: *const u8) -> ! {
+struct Syscon {
+    base: *mut u32,
+    shutdown_magic: u32,
+    restart_magic: u32,
+}
+
+impl Syscon {
+    pub unsafe fn init_with_magic(
+        base: *mut u32,
+        shutdown_magic: u32,
+        restart_magic: u32,
+    ) -> Self {
+        Self {
+            base,
+            shutdown_magic,
+            restart_magic,
+        }
+    }
+
+    pub fn shutdown(&self) -> ! {
+        unsafe { self.base.write_volatile(self.shutdown_magic) };
+        halt()
+    }
+
+    pub fn restart(&self) -> ! {
+        unsafe { self.base.write_volatile(self.restart_magic) };
+        halt()
+    }
+}
+
+unsafe fn locate_syscon(dt: &DevTree) -> Option<Syscon> {
+    let node = dt
+        .compatible_nodes("syscon")
+        .iterator()
+        .next()
+        .and_then(|n| n.ok())?;
+    // TODO: assuming u64 addresses and sizes
+    let base = node
+        .props()
+        .iterator()
+        .find_map(|prop| prop.ok().filter(|p| p.name().is_ok_and(|n| n == "reg")))
+        .and_then(|prop| prop.u64(0).ok())?;
+    // TODO: search device tree for these
+    let shutdown_magic = 0x5555;
+    let restart_magic = 0x7777;
+    unsafe {
+        Some(Syscon::init_with_magic(
+            base as *mut u32,
+            shutdown_magic,
+            restart_magic,
+        ))
+    }
+}
+
+extern "C" fn kmain(hartid: usize, fdt: *const u8, sp: usize) -> ! {
     println!(
-        "\n\nHello world from kernel hart {hartid}!\ntp: {:#x}\nfdt_addr: {fdt:?}\n_kernel_end: {:?}",
-        riscv::r_tp(),
+        "\n\nHello world from kernel hart {hartid}!\n_kernel_end: {:?}\nsp: {sp:#x}\nKSTACK: {:?}\nfdt: {:?}",
         unsafe { addr_of!(_kernel_end) },
+        unsafe { addr_of!(KSTACK) },
+        fdt
     );
 
     // static mut BUFFER: [u8; 0x4000] = [0; 0x4000];
-    // let dt = unsafe { DevTree::from_raw_pointer(fdt) }.unwrap();
+    let dt = unsafe { DevTree::from_raw_pointer(fdt) }.unwrap();
     // dump_fdt::dump_tree(dt, unsafe { &mut BUFFER[..] }).unwrap();
+    println!("Loaded device tree");
 
     // println!("\n----");
     // loop {
@@ -87,8 +147,32 @@ extern "C" fn kmain(hartid: usize, fdt: *const u8) -> ! {
     //     }
     // }
 
+    let syscon = &unsafe { locate_syscon(&dt) };
+    if let Some(syscon) = syscon {
+        println!("Syscon compatible device found at {:?}", syscon.base);
+    } else {
+        println!("No syscon compatible device found. Shutdown will spin.");
+    }
+
     let mut cmd = ArrayString::<256>::new();
     let mut buf = ArrayVec::<u8, 4>::new();
+    let process_cmd = |cmd: &mut ArrayString<256>| {
+        println!("\nCommand: '{cmd}'");
+        if cmd == "exit" {
+            match syscon {
+                Some(s) => s.shutdown(),
+                None => halt(),
+            }
+        } else if cmd == "restart" {
+            match syscon {
+                Some(s) => s.restart(),
+                None => halt(),
+            }
+        }
+        cmd.clear();
+        print!(">> ");
+    };
+
     print!("\n>> ");
     loop {
         let ch = loop {
@@ -98,12 +182,11 @@ extern "C" fn kmain(hartid: usize, fdt: *const u8) -> ! {
         };
         match ch {
             0x0d => {
-                print!("\nCommand: '{cmd}'\n>> ");
-                cmd.clear();
+                process_cmd(&mut cmd);
                 buf.clear();
             }
             0x7f => {
-                /* carriage return (sent on backspace) */
+                /* DEL */
                 if !cmd.is_empty() {
                     cmd.truncate(cmd.len() - cmd.chars().last().map(|c| c.len_utf8()).unwrap_or(0));
                     print!("\x08 \x08");
@@ -128,8 +211,8 @@ extern "C" fn kmain(hartid: usize, fdt: *const u8) -> ! {
                 } else if let Ok(s) = core::str::from_utf8(&buf) {
                     print!("{s}");
                     if cmd.try_push_str(s).is_err() {
-                        print!("\nToo long! Command: '{cmd}'\n>> ");
-                        cmd.clear();
+                        print!("\nToo long!");
+                        process_cmd(&mut cmd);
                     }
                     buf.clear();
                 }
